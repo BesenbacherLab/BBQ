fix: wrong sign on BF QUALs

src/betterbasequals/somatic_callers.py
@@ -10,121 +10,79 @@ def GQ_sum(alt_BQs, ref_BQs):
 
 def get_LR(base_probs):
     #base_probs = [(P(A -> X_read_i|read_i),P(R -> X_read_i|read_i), ..., ]
-    # It is ok that I use phred scales insted of the usual natural log.
-    # Because the ratio will be the same as log(x)/log(y) == log10(x)/log10(y))
+
     def p_data_given_mut(alpha):
-        return sum(p2phred(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) for p_a2x, p_r2x, _ in base_probs)
+        return sum(math.log(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) for p_a2x, p_r2x, _ in base_probs)
         
         # If probabilities in base_probs are not phred scaled. It becomes this:
         #return sum(p2phred(alpha * p_a2x + (1-alpha)*p_r2x) for p_a2x, p_r2x in base_probs)
         
-        #Maybe we can weigh the read prob using the mapping quality
-        #return sum(p2phred((1-p_map_error)*(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) +p_map_error*0.25) for p_a2x, p_r2x, p_map_error in base_probs)
-
     res = minimize_scalar(p_data_given_mut, bounds=(0, 1), method='bounded')
-    # Alternative:
-    # Integrate out alpha (maybe I should call the getBF (Bayes Factor) instead of getLR then)
-    # LL, error = scipy.integrate.quad(p_data_given_mut, 0, 1)
-    # Bør nok have prior fordeling på alpha så.
-    N = len(base_probs)
-    N_A = sum(1 for x,y,_ in base_probs if x<y)
-    alpha = res.x
 
-    #if N_A > 1:
-    #    print(base_probs)
-    #    print([p2phred(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) for p_a2x, p_r2x in base_probs])
-    #    eprint(f'alpha={res.x} N={N} N_A={N_A}')
-    #    print(res.fun, sum(p_r2x for p_a2x, p_r2x in base_probs),  res.fun - sum(p_r2x for p_a2x, p_r2x in base_probs))
-    #LR = res.fun - sum(p2phred((1-p_map_error)*phred2p(p_r2x)+p_map_error*0.25) for p_a2x, p_r2x, p_map_error in base_probs)
-    LR = res.fun - sum(p_r2x for p_a2x, p_r2x, _ in base_probs)
-    return LR, N, N_A, alpha
+    alpha = res.x
+    LR = res.fun - p_data_given_mut(0)
+    return LR, alpha
 
 def get_LR_with_MQ(base_probs):
     #base_probs = [(P(A -> X_read_i|read_i),P(R -> X_read_i|read_i), ..., ]
-    # It is ok that I use phred scales insted of the usual natural log.
-    # Because the ratio will be the same as log(x)/log(y) == log10(x)/log10(y))
+
     def p_data_given_mut(alpha):
         #return sum(p2phred(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) for p_a2x, p_r2x in base_probs)
-        return sum(p2phred((1-phred2p(p_map_error))*(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) + phred2p(p_map_error)*0.5) for p_a2x, p_r2x, p_map_error in base_probs)
+        return sum(math.log((1-phred2p(p_map_error))*(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) + phred2p(p_map_error)*0.5) for p_a2x, p_r2x, p_map_error in base_probs)
 
     res = minimize_scalar(p_data_given_mut, bounds=(0, 1), method='bounded')
-
-    N = len(base_probs)
-    N_A = sum(1 for x,y,_ in base_probs if x<y)
     alpha = res.x
-
-    LR = res.fun - sum(p2phred((1-phred2p(p_map_error))*phred2p(p_r2x)+phred2p(p_map_error)*0.25) for p_a2x, p_r2x, p_map_error in base_probs)
-    return LR, N, N_A, alpha
+    LR = res.fun - p_data_given_mut(0)
+    return LR, alpha
 
 
 def get_BF(base_probs):
     #base_probs = [(P(A -> X_read_i|read_i),P(R -> X_read_i|read_i), ..., ]
-    # It is ok that I use phred scales insted of the usual natural log.
-    # Because the ratio will be the same as log(x)/log(y) == log10(x)/log10(y))
+
     def p_data_given_mut(alpha):
         return phred2p(sum(p2phred(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) for p_a2x, p_r2x, _ in base_probs))
         
-        # If probabilities in base_probs are not phred scaled. It becomes this:
-        #return sum(p2phred(alpha * p_a2x + (1-alpha)*p_r2x) for p_a2x, p_r2x in base_probs)
-        
-        #Maybe we can weigh the read prob using the mapping quality
-        #return sum(p2phred((1-p_map_error)*(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) +p_map_error*0.25) for p_a2x, p_r2x, p_map_error in base_probs)
-
-    #res = minimize_scalar(p_data_given_mut, bounds=(0, 1), method='bounded')
-    # Alternative:
-    # Integrate out alpha (maybe I should call the getBF (Bayes Factor) instead of getLR then)
+    # Integrate out alpha 
     p_no_alpha, error = scipy.integrate.quad(p_data_given_mut, 0, 1)
-    # Bør nok have prior fordeling på alpha så.
-    N = len(base_probs)
-    N_A = sum(1 for x,y,_ in base_probs if x<y)
 
-    #if N_A > 1:
-    #    print(base_probs)
-    #    print([p2phred(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) for p_a2x, p_r2x in base_probs])
-    #    eprint(f'alpha={res.x} N={N} N_A={N_A}')
-    #    print(res.fun, sum(p_r2x for p_a2x, p_r2x in base_probs),  res.fun - sum(p_r2x for p_a2x, p_r2x in base_probs))
-    #LR = res.fun - sum(p2phred((1-p_map_error)*phred2p(p_r2x)+p_map_error*0.25) for p_a2x, p_r2x, p_map_error in base_probs)
     try:
         log_BF = math.log(p_no_alpha) - math.log(p_data_given_mut(0))
     except:
-        log_BF = 0
-    return log_BF, N, N_A, N_A/N
+        log_BF = -1000
+
+    return log_BF
 
 def get_BF_with_MQ(base_probs):
     #base_probs = [(P(A -> X_read_i|read_i),P(R -> X_read_i|read_i), ..., ]
-    # It is ok that I use phred scales insted of the usual natural log.
-    # Because the ratio will be the same as log(x)/log(y) == log10(x)/log10(y))
+
     def p_data_given_mut(alpha):
         return phred2p(sum(p2phred((1-phred2p(p_map_error))*(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) + phred2p(p_map_error)*0.5) for p_a2x, p_r2x, p_map_error in base_probs))
 
     # Integrate out alpha
     p_no_alpha, error = scipy.integrate.quad(p_data_given_mut, 0, 1)
-    # Bør nok have prior fordeling på alpha så.
-    N = len(base_probs)
-    N_A = sum(1 for x,y,_ in base_probs if x<y)
+
     try:
         log_BF = math.log(p_no_alpha) - math.log(p_data_given_mut(0))
     except:
-        log_BF = 0
-    return log_BF, N, N_A, N_A/N
+        log_BF = -1000
 
-def get_BF_with_MQ_and_Prior(base_probs, a=1.1, b=10):
+    return log_BF
+
+def get_BF_with_MQ_and_Prior(base_probs, a=1.05, b=3):
     #base_probs = [(P(A -> X_read_i|read_i),P(R -> X_read_i|read_i), ..., ]
-    # It is ok that I use phred scales insted of the usual natural log.
-    # Because the ratio will be the same as log(x)/log(y) == log10(x)/log10(y))
+
     def p_data_given_mut(alpha):
         return scipy.stats.beta.pdf(alpha, a, b) * phred2p(sum(p2phred((1-phred2p(p_map_error))*(alpha * phred2p(p_a2x) + (1-alpha)*phred2p(p_r2x)) + phred2p(p_map_error)*0.5) for p_a2x, p_r2x, p_map_error in base_probs))
 
     # Integrate out alpha
     p_no_alpha, error = scipy.integrate.quad(p_data_given_mut, 0, 1)
-    # Bør nok have prior fordeling på alpha så.
-    N = len(base_probs)
-    N_A = sum(1 for x,y,_ in base_probs if x<y)
+
     try:
-        log_BF = math.log(p_no_alpha) - math.log(p_data_given_mut(0))
+        log_BF =  math.log(p_no_alpha) - math.log(p_data_given_mut(0))
     except:
-        log_BF = 0
-    return log_BF, N, N_A, N_A/N
+        log_BF = - 1000
+
+    return log_BF
 
 
 class SomaticMutationCaller:
@@ -308,19 +266,26 @@ class SomaticMutationCaller:
                         continue
                     else:
                         F_list.append("lowMQ")
-                if self.method == 'LR':
-                    LR, N, N_A, AF = get_LR(base_probs[A])
-                elif self.method == 'LR_with_MQ':
-                    LR, N, N_A, AF = get_LR_with_MQ(base_probs[A])
-                elif self.method == 'BF':
-                    LR, N, N_A, AF = get_BF(base_probs[A])
-                elif self.method == 'BF_with_MQ':
-                    LR, N, N_A, AF = get_BF_with_MQ(base_probs[A])
-                elif self.method == 'BF_with_MQ_and_Prior':
-                    LR, N, N_A, AF = get_BF_with_MQ_and_Prior(base_probs[A])
 
-                QUAL = -LR
+                AF = None
+                if self.method == 'LR':
+                    QUAL, AF = get_LR(base_probs[A])
+                elif self.method == 'LR_with_MQ':
+                    QUAL, AF = get_LR_with_MQ(base_probs[A])
+                elif self.method == 'BF':
+                    QUAL = get_BF(base_probs[A])
+                elif self.method == 'BF_with_MQ':
+                    QUAL = get_BF_with_MQ(base_probs[A])
+                elif self.method == 'BF_with_MQ_and_Prior':
+                    QUAL = get_BF_with_MQ_and_Prior(base_probs[A])
+
                 if self.cutoff is None or QUAL >= self.cutoff:
+
+                    N = len(base_probs[A])
+                    N_A = sum(1 for x,y,_ in base_probs[A] if x<y)
+                    if AF is None:
+                        AF = N_A/N
+
                     oldBQ_str = '[' + ','.join(x[-1] for x in BQs[A]) + ']'
                     newBQ = '[' +','.join(f'{x[1]:.1f}' for x in BQs[A]) + ']'
                     