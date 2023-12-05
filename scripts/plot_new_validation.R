9306e2e07f26ba758bc70eb9565c3a657a07581e
besenbacher <besenbacher@clin.au.dk>
2023-12-05 23:17:35 +0100

    feat: more plots

--- scripts/plot_new_validation.R
+++ scripts/plot_new_validation.R
@@ -33,13 +33,15 @@ p <- d |>
   group_by(BQ) |>
   summarise(n_seen = sum(as.integer(validation==1)*count),
             n_notseen = sum(as.integer(validation==0)*count)) |>
-  filter(n_seen+n_notseen > 50) |>
+  filter(n_seen+n_notseen > 50,
+         n_seen > 0) |>
   mutate(rate = n_seen/(n_seen+n_notseen),
          lower = binconf(n_seen, n_seen+n_notseen)[,2],
          upper = binconf(n_seen, n_seen+n_notseen)[,3]) |>
-  ggplot(aes(x=BQ, y=rate, ymin=lower, ymax=upper)) + 
-  #geom_ribbon(alpha=0.5) +
-  geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
+  ggplot(aes(x=BQ, y=rate, ymin=lower, ymax=upper)) +
+  geom_ribbon(alpha=0.5) +
+  geom_point() + geom_line() +
+  scale_y_log10("Probability allele seen at same site in HiFi data") +
   theme_bw() 
 
 ggsave(paste(output_prefix, "no_mismatch_BQ.png", sep="_"), p, width=10, height=6)
@@ -71,6 +73,48 @@ p <- d |>
   #filter(max_oldBQ==37) |>
   filter(n_mismatch==0) |> 
   mutate(BQ = as.integer(newBQ/BQ_bin)*BQ_bin) |>
+  group_by(BQ, oldBQ) |>
+  summarise(n_seen = sum(as.integer(validation==1)*count),
+            n_notseen = sum(as.integer(validation==0)*count)) |>
+  filter(n_seen+n_notseen > 50,
+         n_seen>0) |>
+  mutate(rate = n_seen/(n_seen+n_notseen),
+         lower = binconf(n_seen, n_seen+n_notseen)[,2],
+         upper = binconf(n_seen, n_seen+n_notseen)[,3]) |>
+  ggplot(aes(x=BQ, y=rate, color=oldBQ, fill=oldBQ, ymin=lower, ymax=upper)) + 
+  #geom_ribbon(alpha=0.5) +
+  geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
+  theme_bw() 
+
+ggsave(paste(output_prefix, "no_mismatch_BQ_oldBQ.png", sep="_"), p, width=10, height=6)
+
+p <- d |>
+  filter(NA37<10) |>
+  #filter(max_oldBQ==37) |>
+  filter(n_mismatch==0) |> 
+  mutate(BQ = as.integer(newBQ/BQ_bin)*BQ_bin,
+         max_oldBQ = factor(max_oldBQ)) |>
+  group_by(BQ, max_oldBQ) |>
+  summarise(n_seen = sum(as.integer(validation==1)*count),
+            n_notseen = sum(as.integer(validation==0)*count)) |>
+  filter(n_seen+n_notseen > 50,
+         n_seen>0) |>
+  mutate(rate = n_seen/(n_seen+n_notseen),
+         lower = binconf(n_seen, n_seen+n_notseen)[,2],
+         upper = binconf(n_seen, n_seen+n_notseen)[,3]) |>
+  ggplot(aes(x=BQ, y=rate, color=max_oldBQ, fill=max_oldBQ, ymin=lower, ymax=upper)) + 
+  #geom_ribbon(alpha=0.5) +
+  geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
+  theme_bw() 
+
+ggsave(paste(output_prefix, "no_mismatch_BQ_maxoldBQ.png", sep="_"), p, width=10, height=6)
+
+
+p <- d |>
+  filter(NA37<10) |>
+  #filter(max_oldBQ==37) |>
+  filter(n_mismatch==0) |> 
+  mutate(BQ = as.integer(newBQ/BQ_bin)*BQ_bin) |>
   group_by(muttype, oldBQ, BQ, validation) |>
   summarise(count = sum(count)) |>
   group_by(muttype, oldBQ) |>
@@ -78,12 +122,12 @@ p <- d |>
             n_notseen = sum(as.integer(validation==0)*count),
             BQ = sum(BQ*count)/sum(count)) |>
   filter(n_seen+n_notseen > 50,
-         n_seen>0) |>
+         n_seen > 0) |>
   mutate(rate = n_seen/(n_seen+n_notseen),
          lower = binconf(n_seen, n_seen+n_notseen)[,2],
          upper = binconf(n_seen, n_seen+n_notseen)[,3]) |>
-  ggplot(aes(x=BQ, y=rate, color=muttype, shape=oldBQ, fill=muttype, ymin=lower, ymax=upper)) + 
-  #geom_ribbon(alpha=0.5) +
+  ggplot(aes(x=BQ, y=rate, color=oldBQ, shape=muttype, fill=muttype, ymin=lower, ymax=upper)) + 
+  geom_errorbar(alpha=0.5, width=0.2) +    
   geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
   theme_bw()
 
@@ -99,12 +143,13 @@ p <- d |>
   summarise(n_seen = sum(as.integer(validation==1)*count),
             n_notseen = sum(as.integer(validation==0)*count),
             BQ = sum(BQ*count)/sum(count)) |>
-  filter(n_seen+n_notseen > 50) |>
+  filter(n_seen+n_notseen > 50,
+         n_seen >0) |>
   mutate(rate = n_seen/(n_seen+n_notseen),
          lower = binconf(n_seen, n_seen+n_notseen)[,2],
          upper = binconf(n_seen, n_seen+n_notseen)[,3]) |>
-  ggplot(aes(x=BQ, y=rate, color=oldBQ, fill=oldBQ, ymin=lower, ymax=upper)) + 
-  geom_errorbar(alpha=0.5) + geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
+  ggplot(aes(x=BQ, y=rate, color=oldBQ, fill=oldBQ, ymin=lower, ymax=upper)) +
+  geom_errorbar(alpha=0.5, width=0.2) + geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
   theme_bw() 
 
 ggsave(paste(output_prefix, "no_mismatch_oldBQ.png", sep="_"), p, width=7, height=6)
@@ -119,12 +164,13 @@ p <- d |>
   summarise(n_seen = sum(as.integer(validation==1)*count),
             n_notseen = sum(as.integer(validation==0)*count),
             BQ = sum(BQ*count)/sum(count)) |>
-  filter(n_seen+n_notseen > 50) |>
+  filter(n_seen+n_notseen > 50,
+         n_seen>0) |>
   mutate(rate = n_seen/(n_seen+n_notseen),
          lower = binconf(n_seen, n_seen+n_notseen)[,2],
          upper = binconf(n_seen, n_seen+n_notseen)[,3]) |>
   ggplot(aes(x=BQ, y=rate, color=muttype, fill=muttype, ymin=lower, ymax=upper)) + 
-  geom_ribbon(alpha=0.5) + geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
+  geom_errorbar(alpha=0.5, width=0.2) + geom_point() + geom_line() + scale_y_log10("Probability allele seen at same site in HiFi data") +
   theme_bw() 
 
 ggsave(paste(output_prefix, "no_mismatch_muttype.png", sep="_"), p, width=7, height=6)
